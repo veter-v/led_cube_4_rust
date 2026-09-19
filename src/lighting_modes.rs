29e1c70169a941eda2207c1e3e5f27fc6cebf7b5
veter-v <vikylya1709@gmail.com>
2026-09-19 19:29:04 +0300

    feat: add simple mode

---
+++ src/lighting_modes.rs
@@ -0,0 +1,12 @@
+pub trait Mode {
+    fn start_mode();
+}
+
+pub struct AllLeds {
+}
+
+impl Mode for AllLeds {
+    fn start_mode() {
+        todo!("Необходимо реализовать millis, для реализации метода")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod lighting_modes;
+
+use arduino_hal::delay_ms;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,10 +11,38 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    let mut led = pins.d13.into_output();
+    let mut layer_pins = [
+        pins.a0.into_output().downgrade(),
+        pins.a1.into_output().downgrade(),
+        pins.a2.into_output().downgrade(),
+        pins.a3.into_output().downgrade(),
+    ];
+
+    let mut column_pins = [
+        pins.a4.into_output().downgrade(),
+        pins.a5.into_output().downgrade(),
+        pins.d0.into_output().downgrade(),
+        pins.d1.into_output().downgrade(),
+        pins.d2.into_output().downgrade(),
+        pins.d3.into_output().downgrade(),
+        pins.d4.into_output().downgrade(),
+        pins.d5.into_output().downgrade(),
+        pins.d6.into_output().downgrade(),
+        pins.d7.into_output().downgrade(),
+        pins.d8.into_output().downgrade(),
+        pins.d9.into_output().downgrade(),
+        pins.d10.into_output().downgrade(),
+        pins.d11.into_output().downgrade(),
+        pins.d12.into_output().downgrade(),
+        pins.d13.into_output().downgrade(),
+    ];
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        // пока временный цикл с delay или как делать не надо!
+
+        for pin in &mut layer_pins {
+            pin.toggle();
+        }
+        delay_ms(2000);
     }
 }
